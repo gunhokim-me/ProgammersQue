db5d39ee6666cc23e6131dfe0c0e38a10527885e
GunHo Kim <70114446+gunhokim-me@users.noreply.github.com>
2021-01-22 08:37:35 +0900

    프로그래머스 2016년 문제

---
+++ src/level1/2016Year.java
@@ -0,0 +1,212 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        		switch(a){
+				case 1 : 
+			if(b%7 == 1){
+				answer = "FRI";
+			}else if(b%7 == 2){
+				answer = "SAT";
+			}else if(b%7 == 3){
+				answer = "SUN";
+			}else if(b%7 == 4){
+				answer = "MON";
+			}else if(b%7 == 5){
+				answer = "TUE";
+			}else if(b%7 == 6){
+				answer = "WED";
+			}else if(b%7 == 0){
+				answer = "THU";
+			}
+			break;
+		case 2 : 
+			if(b%7 == 5){
+				answer = "FRI";
+			}else if(b%7 == 6){
+				answer = "SAT";
+			}else if(b%7 == 0){
+				answer = "SUN";
+			}else if(b%7 == 1){
+				answer = "MON";
+			}else if(b%7 == 2){
+				answer = "TUE";
+			}else if(b%7 == 3){
+				answer = "WED";
+			}else if(b%7 == 4){
+				answer = "THU";
+			}
+			break;
+		case 3 : 
+			if(b%7 == 4){
+				answer = "FRI";
+			}else if(b%7 == 5){
+				answer = "SAT";
+			}else if(b%7 == 6){
+				answer = "SUN";
+			}else if(b%7 == 0){
+				answer = "MON";
+			}else if(b%7 == 1){
+				answer = "TUE";
+			}else if(b%7 == 2){
+				answer = "WED";
+			}else if(b%7 == 3){
+				answer = "THU";
+			}
+			break;
+		case 4 : 
+			if(b%7 == 1){
+				answer = "FRI";
+			}else if(b%7 == 2){
+				answer = "SAT";
+			}else if(b%7 == 3){
+				answer = "SUN";
+			}else if(b%7 == 4){
+				answer = "MON";
+			}else if(b%7 == 5){
+				answer = "TUE";
+			}else if(b%7 == 6){
+				answer = "WED";
+			}else if(b%7 == 7){
+				answer = "THU";
+			}
+			break;
+		case 5 : 
+			if(b%7 == 6){
+				answer = "FRI";
+			}else if(b%7 == 0){
+				answer = "SAT";
+			}else if(b%7 == 1){
+				answer = "SUN";
+			}else if(b%7 == 2){
+				answer = "MON";
+			}else if(b%7 == 3){
+				answer = "TUE";
+			}else if(b%7 == 4){
+				answer = "WED";
+			}else if(b%7 == 5){
+				answer = "THU";
+			}
+			break;
+		case 6 : 
+			if(b%7 == 3){
+				answer = "FRI";
+			}else if(b%7 == 4){
+				answer = "SAT";
+			}else if(b%7 == 5){
+				answer = "SUN";
+			}else if(b%7 == 6){
+				answer = "MON";
+			}else if(b%7 == 0){
+				answer = "TUE";
+			}else if(b%7 == 1){
+				answer = "WED";
+			}else if(b%7 == 2){
+				answer = "THU";
+			}
+			break;
+		case 7 : 
+			if(b%7 == 1){
+				answer = "FRI";
+			}else if(b%7 == 2){
+				answer = "SAT";
+			}else if(b%7 == 3){
+				answer = "SUN";
+			}else if(b%7 == 4){
+				answer = "MON";
+			}else if(b%7 == 5){
+				answer = "TUE";
+			}else if(b%7 == 6){
+				answer = "WED";
+			}else if(b%7 == 7){
+				answer = "THU";
+			}
+			break;
+		case 8 : 
+			if(b%7 == 5){
+				answer = "FRI";
+			}else if(b%7 == 6){
+				answer = "SAT";
+			}else if(b%7 == 0){
+				answer = "SUN";
+			}else if(b%7 == 1){
+				answer = "MON";
+			}else if(b%7 == 2){
+				answer = "TUE";
+			}else if(b%7 == 3){
+				answer = "WED";
+			}else if(b%7 == 4){
+				answer = "THU";
+			}
+			break;
+		case 9 : 
+			if(b%7 == 2){
+				answer = "FRI";
+			}else if(b%7 == 3){
+				answer = "SAT";
+			}else if(b%7 == 4){
+				answer = "SUN";
+			}else if(b%7 == 5){
+				answer = "MON";
+			}else if(b%7 == 6){
+				answer = "TUE";
+			}else if(b%7 == 0){
+				answer = "WED";
+			}else if(b%7 == 1){
+				answer = "THU";
+			}
+			break;
+		case 10 : 
+			if(b%7 == 0){
+				answer = "FRI";
+			}else if(b%7 == 1){
+				answer = "SAT";
+			}else if(b%7 == 2){
+				answer = "SUN";
+			}else if(b%7 == 3){
+				answer = "MON";
+			}else if(b%7 == 4){
+				answer = "TUE";
+			}else if(b%7 == 5){
+				answer = "WED";
+			}else if(b%7 == 6){
+				answer = "THU";
+			}
+			break;
+		case 11 : 
+			if(b%7 == 4){
+				answer = "FRI";
+			}else if(b%7 == 5){
+				answer = "SAT";
+			}else if(b%7 == 6){
+				answer = "SUN";
+			}else if(b%7 == 0){
+				answer = "MON";
+			}else if(b%7 == 1){
+				answer = "TUE";
+			}else if(b%7 == 2){
+				answer = "WED";
+			}else if(b%7 == 3){
+				answer = "THU";
+			}
+			break;
+		case 12 : 
+			if(b%7 == 2){
+				answer = "FRI";
+			}else if(b%7 == 3){
+				answer = "SAT";
+			}else if(b%7 == 4){
+				answer = "SUN";
+			}else if(b%7 == 5){
+				answer = "MON";
+			}else if(b%7 == 6){
+				answer = "TUE";
+			}else if(b%7 == 0){
+				answer = "WED";
+			}else if(b%7 == 1){
+				answer = "THU";
+			}
+			break;
+		}
+        return answer;
+    }
+}
